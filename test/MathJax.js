11e5b4d5ef12b71142158b669f9ceb6548853ec9
Peter Krautzberger <p.krautzberger@gmail.com>
2017-04-12 17:02:38 +0200

    add testing helper

---
+++ test/MathJax.js
@@ -0,0 +1,19 @@
+
+(function () {
+  var newMathJax = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js';
+  var oldMathJax = 'https://rawgit.com/mathjax/cdn-redirect/master/test/MathJax.js';
+  var n = oldMathJax.length;
+
+  var scripts = document.getElementsByTagName('script');
+  for (var i = 0; i < scripts.length; i++) {
+    var script = scripts[i];
+    var src = script.getAttribute('src') || '';
+    if (src.substr(0, n) === oldMathJax) {
+      var newScript = document.createElement('script');
+      newScript.src = newMathJax + src.substr(n);
+      script.parentNode.replaceChild(newScript, script);
+      console.warn('WARNING: cdn.mathjax.org has been retired. Check https://www.mathjax.org/cdn-shutting-down/ for migration tips.')
+      break;
+    }
+  }
+})();
