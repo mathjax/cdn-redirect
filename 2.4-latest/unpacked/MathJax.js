c96d20b7766bf9f0b216cf2e9daaea187386f6d0
Peter Krautzberger <p.krautzberger@gmail.com>
2017-04-12 10:20:19 +0200

    first commit

---
+++ 2.4-latest/unpacked/MathJax.js
@@ -0,0 +1,19 @@
+
+(function () {
+  var newMathJax = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.4.0/MathJax.js';
+  var oldMathJax = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js';
+  var n = oldMathJax.length;
+
+  var scripts = document.getElementsByTagName('script');
+  for (var i = 0; i < scripts.length; i++) {
+    var script = scripts[i];
+    var src = script.getAttribute('src') || '';
+    if (src.substr(0, n) === oldMathJax) {
+      var newScript = document.createElement('script');
+      newScript.src = newMathJax + src.substr(n);
+      script.parentNode.replaceChild(newScript, script);
+      console.log('WARNING: cdn.mathjax.org has been retired. Check https://www.mathjax.org/cdn-shutting-down/ for migration tips.')
+      break;
+    }
+  }
+})();
---
+++ README.md
@@ -0,0 +1,3 @@
+# cdn-stopgap 
+
+This repository contains resources that will be pushed to the CDN on May 1 to redirect calls to `cdn.mathjax.org` to `cdnjs.cloudflare.com`.
