a1d5196480d41ef9de680bc71471ead27daa6c4c
Peter Krautzberger <p.krautzberger@gmail.com>
2017-04-18 17:29:24 +0200

    protocol support; optimize using currentScript

--- 2.5-latest/unpacked/MathJax.js
+++ 2.5-latest/unpacked/MathJax.js
@@ -1,19 +1,26 @@
-
 (function () {
   var newMathJax = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.5.3/MathJax.js';
-  var oldMathJax = 'https://cdn.mathjax.org/mathjax/2.5-latest/unpacked/MathJax.js';
-  var n = oldMathJax.length;
+  var oldMathJax = 'cdn.mathjax.org/mathjax/2.5-latest/unpacked/MathJax.js';
 
-  var scripts = document.getElementsByTagName('script');
-  for (var i = 0; i < scripts.length; i++) {
-    var script = scripts[i];
-    var src = script.getAttribute('src') || '';
-    if (src.substr(0, n) === oldMathJax) {
-      var newScript = document.createElement('script');
-      newScript.src = newMathJax + src.substr(n);
-      script.parentNode.replaceChild(newScript, script);
-      console.warn('WARNING: cdn.mathjax.org has been retired. Check https://www.mathjax.org/cdn-shutting-down/ for migration tips.')
-      break;
+  var replaceScript = function (script, rawSrc) {
+    var newScript = document.createElement('script');
+    newScript.src = newMathJax + rawSrc.substr(n);
+    script.parentNode.replaceChild(newScript, script);
+    console.warn('WARNING: cdn.mathjax.org has been retired. Check https://www.mathjax.org/cdn-shutting-down/ for migration tips.')
+  }
+  if (document.currentScript) {
+    var script = document.currentScript;
+    replaceScript(script, script.src.replace(/^(https?:)?\/\//i, ''));
+  } else {
+    var scripts = document.getElementsByTagName('script');
+    for (var i = 0; i < scripts.length; i++) {
+      var script = scripts[i];
+      var src = script.getAttribute('src') || '//';
+      var rawSrc = src.split('//')[1];
+      if (rawSrc.substr(0, n) === oldMathJax) {
+        replaceScript(script, rawSrc);
+        break;
+      }
     }
   }
 })();
--- test/MathJax.js
+++ test/MathJax.js
@@ -1,7 +1,6 @@
 (function () {
   var newMathJax = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js';
   var oldMathJax = 'rawgit.com/mathjax/cdn-redirect/master/test/MathJax.js';
-  var n = oldMathJax.length;
 
   var replaceScript = function (script, rawSrc) {
     var newScript = document.createElement('script');
